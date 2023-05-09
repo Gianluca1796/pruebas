8a97a820456ac8e42b386bd43c7d8d12793998b6
Gianluca Fava <101229939+Gianluca1796@users.noreply.github.com>
2023-05-09 11:49:09 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+class people {
+    constructor(nombre, edad, altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.altura = altura;
+    }
+}
+
+const people1 = new people("Adrian", 27, 1.90);
+
+console.log(people1);
+
+// class info {
+//     constructor(nombre1, edad1, altura) {
+//         this.nombre1 = nombre1;
+//         this.edad1 = edad1;
+//         this.altura1 = altura1;
+//     }
+// }
